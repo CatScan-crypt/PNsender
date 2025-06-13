9c5aaf3cf16326a117fd3a6f6d57701beaf3d72b
CatScan-crypt <mariroth60@gmail.com>
2025-06-13 16:42:13 +0300

    Change api dir location to solve 404.

--- api/redis.ts
+++ api/redis.ts
@@ -1,8 +1,13 @@
 import { createClient } from 'redis';
 import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 // Explicitly load environment variables from .env.development.local
-dotenv.config({ path: '.env.development.local' });
+// Construct an absolute path to the .env file in the project root
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const envPath = path.resolve(__dirname, '..', '.env.development.local');
+dotenv.config({ path: envPath });
 
 const redisUrl = process.env.REDIS_URL;
 
